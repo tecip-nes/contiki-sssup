4adf7fa279b58e2eb153ffd899f18a68c1d127d5
andreaazzara <andrea.azza@gmail.com>
2014-10-20 15:24:59 +0200

    added example for virtual actuator

--- examples/tres/src/cfg-script/tres-config-vs.sh
+++ examples/tres/src/cfg-script/tres-config-vs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ../../../../apps/tres/tools/tres-client-18 \
    coap://[aaaa::200:0:0:2]/tasks/halve \
-   halve.py \
+   vs.py \
    "<coap://[aaaa::200:0:0:3]/sensor>","<coap://[aaaa::200:0:0:4]/sensor>" \
    "<coap://[aaaa::200:0:0:4]/actuator>","<coap://[aaaa::200:0:0:5]/actuator>"
